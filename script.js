33496e74f190166df81076ed95360d7b1c042675
ma10as69 <mathan35@vgs.nfk.no>
2022-10-03 10:03:39 +0200

    added worker and made price variable

--- script.js
+++ script.js
@@ -2,37 +2,64 @@ burger_press = document.querySelector("#burger");
 burger_num = document.querySelector("#burger_num");
 hand = document.querySelector("#hand");
 hand_num = document.querySelector("#hand_num");
+worker = document.querySelector("#worker");
+worker_num = document.querySelector("#worker_num");
 
 console.log(burger_num);
 
 burger = 0;
 hands = 0;
-seconds = 1000;
+worker = 0;
+seconds_shop = 1000;
+seconds_worker = 1000;
+price_hand = 50;
+
 
 function burger_click(){
     burger++;
 }
 function buy_hand(){
-    if (burger >= 50){
+    if (burger >= price_hand){
         hands++;
-        burger -= 50;
-        seconds -= 100;
-        timer = setInterval(myTimer, seconds); // kjører funksjonen myTimer 1 gang i sekundet
+        burger -= price_hand;
+        seconds_shop -= 100;
+            price_hand *= 2;
+        timer_shop = setInterval(myTimer_shop, seconds_shop); // kjører funksjonen myTimer 1 gang i sekundet
+    
+    }
+    
+}
+function hire_worker(){
+    if (burger >= 10){
+        worker++;
+        burger -= 10;
+        seconds_worker -= 100;
+        timer_worker = setInterval(myTimer_worker, seconds_worker);
     }
     
 }
 
 burger_press.addEventListener("click", burger_click); // når vi klikker på cookie
 hand.addEventListener("click", buy_hand)
+hand.addEventListener("click", hire_worker)
 
 
-var timer = setInterval(myTimer, seconds); // kjører funksjonen myTimer 1 gang i sekundet
-function myTimer(){ 
+var timer_shop = setInterval(myTimer_shop, seconds_shop); // kjører funksjonen myTimer 1 gang i sekundet
+function myTimer_shop(){ 
     if (hands >= 1){
         burger_click();
     }
 
     hand_num.innerHTML = hands;
+}
+
+var timer_worker = setInterval(myTimer_worker, seconds_worker);
+function myTimer_worker(){ 
+    if (worker >= 1){
+        burger_click();
+    }
+
+    hand_num.innerHTML = worker;
 }
 
 
