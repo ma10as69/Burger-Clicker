558cb879ba3ddcefe539b7f80c37b2875274b721
ma10as69 <mathan35@vgs.nfk.no>
2022-10-11 14:38:49 +0200

    11.10.22

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ worker = document.querySelector("#worker");
 worker_num = document.querySelector("#worker_num");
 money = document.querySelector("#money");
 money_num = document.querySelector("#money_num");
+clicks_persec = document.querySelector("#clicks_persec");
+
+
 
 
 console.log(burger_num);
@@ -14,27 +17,28 @@ console.log(burger_num);
 
 burger = 0;
 hands = 0;
-money = 0;
+money_amount = 0;
 worker_amount = 0;
 seconds_shop = 1000;
 seconds_worker = 1000;
 price_hand = 10;
 price_worker = 10;
+clicks_persecond = 0;
+
 
 
 function burger_click(){
     burger++;
-}
-function SellFunction() {
-    sell_burger.disabled = true;
 }
 function buy_hand(){
     if (burger >= price_hand){
         hands++;
         burger -= price_hand;
         seconds_shop -= 100;
-            price_hand *= 2;
+        price_hand *= 2;
+        clicks_persec;
         timer_shop = setInterval(myTimer_shop, seconds_shop); // kjører funksjonen myTimer 1 gang i sekundet
+        clicks_persec.innerHTML = clicks_persecond;
     
     }
     
@@ -44,11 +48,13 @@ function hire_worker(){
         worker_amount++;
         burger -= price_worker;
         seconds_worker -= 100;
-            price_worker *= 2;
+        price_worker *= 2;
         timer_worker = setInterval(myTimer_worker, seconds_worker);
     }
     worker_num.innerHTML = worker_amount;
+  
 }
+
 
 burger_press.addEventListener("click", burger_click); // når vi klikker på cookie
 hand.addEventListener("click", buy_hand)
@@ -67,10 +73,10 @@ function myTimer_shop(){
 var timer_worker = setInterval(myTimer_worker, seconds_worker);
 function myTimer_worker(){ 
     if (worker_amount >= 1){
-        burger_click();
-    }
+        money_amount ++;
+    }  
+    money_num.innerHTML = money_amount;
 
-    
 }
 
 
